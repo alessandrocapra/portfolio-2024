4a733dcf495e80d569e2325ed2bd82fc0d92265c
Alessandro Capra <alessandro.capra@leaseplan.com>
2025-01-21 21:07:06 +0100

    Homepage done

--- portfolio-2024/src/App.tsx
+++ portfolio-2024/src/App.tsx
@@ -2,19 +2,19 @@
 function App() {
   return (
     <div className="flex flex-col gap-8 sm:gap-12">
-      <div className="sm:flex  sm:gap-4">
-        <div className="sm:w-1/2 sm:flex sm:flex-col sm:justify-center lg:w-2/3">
+      <div className="sm:flex sm:flex-row-reverse sm:gap-8">
+        <div className="sm:w-3/4 sm:flex sm:flex-col sm:justify-center">
           <h1 className="font-semibold mb-4">Alessandro Capra</h1>
           <div className="text-md max-w-prose">
             <p>
-              I'm a senior frontend engineer from Italy, lived in Germany, Finland and currently living in the Netherlands
+              I'm a senior frontend engineer from Italy. I've lived in Germany, Finland and currently living in the Netherlands.
             </p>
             <p>
               I strive to <span className="font-bold">write code that turns into value</span>, both for users and the business
             </p>
           </div>
         </div>
-        <img src='/images/hike-01.jpeg' alt='Me hiking in the mountains' className="rounded-lg w-full h-auto object-cover sm:w-1/2 lg:w-1/3" />
+        <img src='/images/hike-01.jpeg' alt='Me hiking in the mountains' className="rounded-lg w-full h-auto object-cover sm:w-1/4" />
       </div>
       <div>
         <h2 className="font-mono pb-4">TLDR;</h2>
