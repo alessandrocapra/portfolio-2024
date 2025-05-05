082bde4971906683c9069fc8acad8718d62d0dc5
Alessandro Capra <alessandro.cpr@gmail.com>
2025-05-05 21:52:32 +0000

    test ssg

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import vitePrerender from 'vite-plugin-prerender'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
     react(),
+    vitePrerender({
+      staticDir: path.join(__dirname, 'dist'),
+      routes: ['/', '/about', '/projects', '/projects/custodian', '/projects/whistleblower', '/projects/gamified-respiratory-exercises', '/projects/babbelbord'],
+    }),
     ViteImageOptimizer({
       png: {
         quality: 80
